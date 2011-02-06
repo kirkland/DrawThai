aa4b3e465c8f948d7634de3cf0ba03280d67a810
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-02-06 17:44:27 -0500

    initial commit

---
+++ src/info/kirklander/draw_thai/DrawThai.java
@@ -0,0 +1,12 @@
+package info.kirklander.draw_thai;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawThai extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
